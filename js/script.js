0155a7742628e9115faad4ce8a56dfa5c06a282a
laz <jannnjan592@gmail.com>
2022-02-02 19:29:16 +0300

    laz

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let headerCarousel1 = document.querySelectorAll('.header__bottom1')[0],
+    headerCarousel2 = document.querySelectorAll('.header__bottom2')[0],
+    headerCarousel3 = document.querySelectorAll('.header__bottom3')[0],
+    headerCicle1 = document.querySelectorAll('.header__Circle1')[0],
+    headerCicle2 = document.querySelectorAll('.header__Circle2')[0],
+    headerCicle3 = document.querySelectorAll('.header__Circle3')[0];
+
+    
+
+
